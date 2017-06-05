80d9e9d50191dbd6c09db662b20b2db0e2c44ba2
David Mak <david.18.19.21@gmail.com>
2017-06-05 22:22:54 +0800

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import kotlin.system.exitProcess
+
+fun outputVersion() {
+    println("Bob 0.0.1")
+    exitProcess(0)
+}
+
+fun outputHelp() {
+    // TODO: Fill in the helptext
+    exitProcess(0)
+}
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,12 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by david on 5/6/2017.
+ */
+
+class MainTest {
+    @Test fun BasicTest() {
+        assertEquals(1, 1)
+    }
+}
